b44297c98c987bac464a0740b8baf9d94893f514
Mohamed Askar <mohamed.askar@epassi.com>
2025-12-29 19:44:06 +0100

    Configure GitHub Pages deployment

--- src/main.tsx
+++ src/main.tsx
@@ -16,7 +16,7 @@ if (localStorage.getItem('invoice-app-version') !== DATA_VERSION) {
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/invoice-app">
       <App />
       <Toaster />
     </BrowserRouter>
